7ba6461d75a296584135c64e7e8f3c451756b2d6
Shreesh <shreesh.dutta@gmail.com>
2022-08-30 15:46:18 +0530

    Partial Classes

---
+++ EmployeeTask/PartialClasses/PartialClass1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EmployeeTask.PartialClasses
+{
+    public partial class Sample
+    {
+        private string movie;
+        private int release_year;
+
+        public Sample(string x, int y)
+        {
+            this.movie = x;
+            this.release_year = y;
+        }
+    }
+}
---
+++ EmployeeTask/PartialClasses/PartialClass2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTask.PartialClasses
+{
+        public partial class Sample
+        {
+            public void displayMovieDetails()
+            {
+                Console.WriteLine("Movie title is: " + movie);
+                Console.WriteLine("Release year is: " + release_year);
+            }
+        }
+    }
