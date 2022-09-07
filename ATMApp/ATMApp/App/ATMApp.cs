659223f795cfa642ea024400e382ca274dffe39b
Shreesh <shreesh.dutta@gmail.com>
2022-09-07 16:47:11 +0530

    ATM App

---
+++ ATMApp/ATMApp/App/ATMApp.cs
@@ -0,0 +1,29 @@
+﻿using ATMApp.Domain.Entities;
+using ATMApp.Domain.Interfaces;
+using ATMApp.UI;
+using System.Collections.Generic;
+
+public class ATMapp : IUserLogin
+
+{
+    private List<UserAccount> userAccountList;
+    private UserAccount selectedAccount;
+
+    public void CheckUserCardNumAndPass()
+    {
+        bool isCorrectLogin = false;
+
+        UserAccount tempUserAccoount = new UserAccount();
+    }
+
+    public void InitialiseData()
+    {
+        userAccountList = new List<UserAccount>
+            {
+                new UserAccount{id=1, FullName="Shreesh Dutta Bajpai", AccountNumber=12345678, CardNumber=431431, CardPin=123456, AccountBalance=50000.00m, isLocked=false},
+                new UserAccount{id=2, FullName="Abhishek Rawat", AccountNumber=09876543, CardNumber=123123, CardPin=567890, AccountBalance=40000.00m, isLocked=false},
+                new UserAccount{id=1, FullName="Sumit Thapliyal", AccountNumber=0987124, CardNumber=567567, CardPin=345678, AccountBalance=30000.00m, isLocked=true}
+            };
+    }
+
+}
---
+++ ATMApp/ATMApp/UI/AppScreen.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.UI
+{
+    public static class AppScreen
+    {
+        internal static void Welcome()
+        {
+            //Clears Console
+            Console.Clear();
+            //Sets the title
+            Console.Title = "My ATM App";
+            //Sets the text color or foreground color to white
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("------------!!!Welccome to My ATM App!!!---------------");
+            Console.WriteLine("Please insert your ATM Card. \n");
+            Console.WriteLine("Note: The actual machine will accept and validate the card.");
+            Utility.PressEnterToContinue();
+        }
+
+        
+    }
+}
