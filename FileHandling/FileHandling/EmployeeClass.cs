6e9f2470ea022c24b304b7b7410112c467715181
Shreesh <shreesh.dutta@gmail.com>
2022-09-09 16:17:57 +0530

    File Handling

---
+++ FileHandling/FileHandling/EmployeeClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileHandling
+{
+    public class EmployeeClass
+    {        
+            public int eid;
+            public string name;
+            public string address;
+    }
+}
---
+++ FileHandling/FileHandling/Program.cs
@@ -0,0 +1,65 @@
+﻿//connection string (db connection) 
+using FileHandling;
+using System.Data.SqlClient;
+
+SqlConnection con = new SqlConnection("server=localhost;database=Bank;integrated security=true");
+string isrepeat = "Y";
+EmployeeClass emp = new EmployeeClass();
+
+string empFile = @"C:\Users\shreesh.bajpai\source\repos\FileHandling\FileHandling.txt";
+
+try
+{
+    while (isrepeat.ToUpper() == "Y")
+    {
+        Console.Write("Enter Employee's ID: ");
+        emp.eid = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter Employee's Name: ");
+        emp.name = Console.ReadLine();
+
+        Console.Write("Enter Employee's Address: ");
+        emp.address = Console.ReadLine();
+
+
+        //insertion command creation
+        SqlCommand cmd = new SqlCommand("insert into Employee values( " + emp.eid + "  , '" + emp.name + "' , ' " + emp.address + " ')", con);
+        con.Open();
+        cmd.ExecuteNonQuery();
+        con.Close();
+        Console.WriteLine("Record Inserted Successfully");
+
+        if (!File.Exists(empFile))
+        {
+            using (StreamWriter sw = File.CreateText(empFile))
+            {
+                sw.WriteLine($"Employee's Id = {emp.eid}, Employee's name = {emp.name}, Employee's Address = {emp.address}");
+            }
+        }
+        else
+        {
+            using (StreamWriter sw = File.AppendText(empFile))
+            {
+                sw.WriteLine($"EId = {emp.eid}, Ename = {emp.name}, Address = {emp.address}");
+            }
+        }
+
+        Console.WriteLine("\n\nThe entries in text files are:\n");
+
+        using (StreamReader sr = File.OpenText(empFile))
+        {
+            string s = "";
+            while ((s = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        Console.WriteLine("Do you want to repeat? Y or N");
+        isrepeat = Console.ReadLine();
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
