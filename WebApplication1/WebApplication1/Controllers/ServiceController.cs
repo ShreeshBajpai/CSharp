e875b119e40959dd4bf60147fe323c6deacf6e64
Shreesh <shreesh.dutta@gmail.com>
2022-09-28 03:21:52 +0530

    Assessment

---
+++ WebApplication1/WebApplication1/Controllers/ServiceController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class ServiceController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
